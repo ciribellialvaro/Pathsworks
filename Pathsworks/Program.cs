58c1cabc68c933e45f4bc43260e255a5386131fd
Álvaro Borges <gestor.alvaro@gmail.com>
2023-01-10 23:07:26 -0300

    Add project files.

---
+++ Pathsworks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pathsworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Memory\GerenciamentoMedicamentos1\GerenciamentoMedicamentos1.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name Without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
